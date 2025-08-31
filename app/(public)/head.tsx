b4f1532d1001c3354070ccbaf00b337bc64332e8
Dominique Foertig <ernst.koslitsch@proton.me>
2025-08-31 20:37:26 +0200

    revert: drop (public)/layout; move robots+RSS to (public)/head to preserve navbar

--- app/(public)/head.tsx
+++ app/(public)/head.tsx
@@ -4,9 +4,7 @@ export default function Head() {
   const base = (SITE_URL ?? "http://localhost:3000").replace(/\/$/,"")
   return (
     <>
-      {/* SEO: große Bildvorschau erlauben */}
       <meta name="robots" content="max-image-preview:large" />
-      {/* RSS Feed für News/Articles */}
       <link
         rel="alternate"
         type="application/rss+xml"
